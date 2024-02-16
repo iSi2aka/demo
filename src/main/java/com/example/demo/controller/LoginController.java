1af98c62a9513f6dbf0e4e4f0807b9c93c31c9a6
iSi2aka <n.ishizaka@outlook.com>
2024-02-16 15:04:31 +0900

    viewの引数を指定

--- src/main/java/com/example/demo/controller/LoginController.java
+++ src/main/java/com/example/demo/controller/LoginController.java
@@ -3,11 +3,14 @@ package com.example.demo.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.example.demo.form.LoginForm;
+
 @Controller
 public class LoginController {
 	
 	@GetMapping("/login")
-	public String view() {
+	public String view(Model model, LoginForm form) {
+		
 		return "login";
 	}
 
