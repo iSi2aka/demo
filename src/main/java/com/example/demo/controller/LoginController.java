c880816e14203d51e873afbd1482029a3686a7d0
iSi2aka <n.ishizaka@outlook.com>
2024-02-16 15:36:19 +0900

    フォーム入力値確認のための記述

--- src/main/java/com/example/demo/controller/LoginController.java
+++ src/main/java/com/example/demo/controller/LoginController.java
@@ -3,6 +3,7 @@ package com.example.demo.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import com.example.demo.form.LoginForm;
 
@@ -13,6 +14,11 @@ public class LoginController {
 	public String view(Model model, LoginForm form) {
 		
 		return "login";
+	}
+	
+	@PostMapping("/login")
+	public void login(LoginForm form) {
+		System.out.println(form.toString());
 	}
 
 }
